419350bd737d7cb3d98b7ff90aec2c1549d25d1b
Willian Franco <ing.wfranco@gmail.com>
2022-04-21 18:28:27 -0500

    adicion funciones --- se adicionana las funciones clean, draw y play al prototipo de boardview

--- main.js
+++ main.js
@@ -1,31 +1,87 @@
-(function(){
-    self.Board = function(width, height){
-    this.width = width;
-    this.height = height;
-    this.playing = false;
-    this.game_over = false;
-    this.bars = [];
-    this.ball = null;
-    this.playing = false;
-}
-
-self.Board.prototype = {
-    get elements(){
-        var elements = this.bars.map(function(bar){return bar;});
-        elements.push(this.ball);
-        return elements;
+(function () {
+    self.Board = function (width, height) {
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+        this.playing = false;
     }
-}
+
+    self.Board.prototype = {
+        get elements() {
+            var elements = this.bars.map(function (bar) { return bar; });
+            elements.push(this.ball);
+            return elements;
+        }
+    }
 })();
 
+(function () {
+    self.Bar = function (x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 10;
+    }
 
-(function(){
-self.BoardView = function(canvas, board){
-    this.canvas = canvas;
-    this.canvas.width = board.width;
-    this.canvas.height = board.height;
-    this.board = board;
-    this.ctx = canvas.getContext("2d");
-}
+    self.Bar.prototype = {
+        down: function () {
+            this.y += this.speed;
+        },
+        up: function () {
+            this.y -= this.speed;
+        },
+        toString: function () {
+            return "x: " + this.x + " y: " + this.y;
+        }
+    }
+})();
+
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype = {
+        clean: function () {
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.ctx, el);
+            };
+        },
+        check_collisions: function () {
+            for (let i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i]
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+
+            }
+        },
+        play: function () {
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.check_collisions();
+                this.board.ball.move();
+            }
+
+        }
+    }
+
 
 })();
