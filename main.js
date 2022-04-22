be14fc9b6500281bfad918ebd76c5b7e1208505d
Willian Franco <ing.wfranco@gmail.com>
2022-04-22 09:41:56 -0500

    app complete -- se adicionan todas las funcionalidades al app y se termina

--- main.js
+++ main.js
@@ -48,20 +48,20 @@
             return this.radius * 2;
 
         },
-        collision : function(bar){
+        collision: function (bar) {
             //Reacciona a la colisiona con una barra que recibe como parametro  
-       var relative_intersect_y = ( bar.y + (bar.height / 2) ) - this.y;
-   
-       var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
-   
-       this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
-   
-       this.speed_y = this.speed * -Math.sin(this.bounce_angle);
-       this.speed_x = this.speed * Math.cos(this.bounce_angle);
-   
-       if (this.x > (this.board.width / 2)) this.direction = -1;
-       else this.direction = 1;
-       }
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
+        }
     }
 
 })();
@@ -130,33 +130,88 @@
 
         }
     }
-    function hit(a, b){
-        //Revisa si a colisiona con b
-var hit = false;
-//Colisiones hirizontales
-if(b.x + b.width >= a.x && b.x < a.x + a.width){
 
-//Colisiona verticales
-if (b.y + b.height >= a.y && b.y < a.y + a.height) 
-    hit = true;
-}
+    function hit(a, b) {
+        
+        var hit = false;
+       
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
 
-//Colisión de a con b
-if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+            
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
 
-if (b.y <= a.y && b.y + b.height >= a.y + a.height) 
-    hit = true;
-}
+        
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
 
-//Colision b con a
-if(a.x <= b.x && a.x + a.width >= b.x + b.width){
-//Colisiona verticales
-if (a.y <= b.y && a.y + a.height >= b.y + b.height) 
-    hit = true;
-}
-return hit;
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+        }
 
-}
+        
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
 
+    }
+
+    function draw(ctx, element) {
+        switch (element.kind) {
+            case "rectangle":
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
+        }
+    }
 
 })();
+
+var board = new Board(800, 400);
+var bar = new Bar(20, 100, 40, 100, board);
+var bar_2 = new Bar(740, 100, 40, 100, board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
+
+
+
+document.addEventListener("keydown", function (ev) {
+
+    if (ev.keyCode == 38) {
+        ev.preventDefault();
+        bar.up();
+    }
+    else if (ev.keyCode == 40) {
+        ev.preventDefault();
+        bar.down();
+    } else if (ev.keyCode == 87) {
+        ev.preventDefault();
+        bar_2.up();
+    } else if (ev.keyCode == 83) {
+        ev.preventDefault();
+        bar_2.down();
+    } else if (ev.keyCode == 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
+    }
+
+});
+
+
+board_view.draw();
+window.requestAnimationFrame(controller);
+
+function controller() {
+    board_view.play();
+    window.requestAnimationFrame(controller);
+
+}
